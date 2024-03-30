ddb0068883d928489ab75438a21a1217f87efe82
makerseok <maker7788@gmail.com>
2024-03-30 11:42:20 +0900

    [Feature] 캐시 초기화

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'site-static-136';
-const DYNAMIC_CACHE_NAME = 'site-dynamic-v127';
+const STATIC_CACHE_NAME = 'site-static-137';
+const DYNAMIC_CACHE_NAME = 'site-dynamic-v128';
 const VIDEO_CACHE_NAME = 'site-video-v4';
 const FONT_CACHE_NAME = 'site-font-v1';
 const APEX_CACHE_NAME = 'site-apex-v1';
